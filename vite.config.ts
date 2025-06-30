5730de79ae5eb716e2a6ed0d691098e741a56b4b
Elfredy <mac@Ferids-MacBook-Pro.local>
2025-07-01 01:43:58 +0400

    logo chansdge

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-   base: '/',
+  base: '/e-commerce-demo/', 
   plugins: [
     tailwindcss(),
     react()
-  ],
-
+  ]
 })
-
